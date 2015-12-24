a5d5ef581c8e2f86e5a07be6443fb1cdf830a9bc
Pooja <pooja.wohlig@gmail.com>
2015-12-24 16:08:39 +0530

    ui foxed for pillar

---
+++ application/views/pillar.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Starter Template for Bootstrap 3.3.6</title>
+    <link rel="shortcut icon" href="">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+  
+    <link href="<?php echo base_url('assets').'/';?>css/materialize.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets').'/';?>css/range.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets').'/';?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets').'/';?>css/materialize.min.css" rel="stylesheet">
+    
+    <!--[if IE]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="yellow-back">
+    <div class="container">
+        <div class="row">
+            <div class="col s6 ">
+                <div class="padtop">
+
+                    <h5 class="padleft">Master Admin Weightages</h5>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Work Life Blend</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range" id="range" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Driving Force</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range1" id="range1" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range1" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Interpersonal Relationships at Work</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range2" id="range2" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range2" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Sens of Ownership</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range3" id="range3" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range3" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Job Security</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range4" id="range4" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range4" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            <div class="col s6 ">
+                <div class="padtop">
+
+                    <h5 class="padleft">HR Admin Weightages</h5>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Work Life Blend</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range5" id="range5" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range5" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Driving Force</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range6" id="range6" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range6" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Interpersonal Relationships at Work</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range7" id="range7" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range7" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Sens of Ownership</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range8" id="range8" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range8" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="all-range">
+                            <div class="col s10">
+                                <div class="left-side">
+
+                                    <div class="progress-bar">
+                                        <p>Job Security</p>
+
+                                        <form action="#">
+                                            <div class="input-border">
+                                                <p class="range-field">
+                                                    <input type="range" name="range9" id="range9" min="0" max="100" />
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s2">
+                                <div class="progress-bar out-top">
+                                    <output for="range9" class="output"></output>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col s12">
+            <div class="text-center">
+            
+<a class="waves-effect waves-light btn blue-btn">OK</a>
+            </div>
+            </div>
+        </div>
+    </div>
+     
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="<?php echo base_url('assets').'/';?>js/index.js"></script>
+ 
+</body>
+
+</html>
--- assets/js/index.js
+++ assets/js/index.js
@@ -9,4 +9,32 @@
 
     $("[name=range2]").on("change", function() {
     $("[for=range2]").val(this.value +"%" );
+    }).trigger("change"); 
+
+    $("[name=range3]").on("change", function() {
+    $("[for=range3]").val(this.value +"%" );
+    }).trigger("change"); 
+
+    $("[name=range4]").on("change", function() {
+    $("[for=range4]").val(this.value +"%" );
+    }).trigger("change");
+
+    $("[name=range5]").on("change", function() {
+    $("[for=range5]").val(this.value +"%" );
+    }).trigger("change");
+
+    $("[name=range6]").on("change", function() {
+    $("[for=range6]").val(this.value +"%" );
+    }).trigger("change");
+
+    $("[name=range7]").on("change", function() {
+    $("[for=range7]").val(this.value +"%" );
+    }).trigger("change");
+
+    $("[name=range8]").on("change", function() {
+    $("[for=range8]").val(this.value +"%" );
+    }).trigger("change");
+
+    $("[name=range9]").on("change", function() {
+    $("[for=range9]").val(this.value +"%" );
     }).trigger("change");
