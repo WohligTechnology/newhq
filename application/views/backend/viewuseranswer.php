9472e6e00ecb92bbc32c1196fa4560875f7459f2
Pooja <pooja.wohlig@gmail.com>
2016-01-12 10:53:44 +0530

    password

--- application/views/backend/viewuseranswer.php
+++ application/views/backend/viewuseranswer.php
@@ -2,7 +2,7 @@
     <div class="col s12"><div class="row">
 
             <div class="col s12 drawchintantable">
-               <?php $this->chintantable->createsearch('List of Useranswer');?>
+               <?php $this->chintantable->createsearch('User Answers');?>
                 <table class="highlight responsive-table">
                     <thead>
                         <tr>
@@ -21,53 +21,32 @@
             </div>
     </div>
         <?php $this->chintantable->createpagination();?>
-
-    </div>
-    <div class="createbuttonplacement"><a class="btn-floating btn-large waves-effect waves-light blue darken-4 tooltipped" href="<?php echo site_url('site/createuseranswer'); ?>"data-position="top" data-delay="50" data-tooltip="Create"><i class="material-icons">add</i></a>
     </div>
 
+</div>
+<div style="position:fixed;width:100%;height:100%;left:0;top:0;background:white;z-index:1000;" class="passwordFixed">
+    <div class="container">
+        <form role="form" style="padding:20%;" class="passwordForm">
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control passwordOnly" id="pwd">
+          </div>
+          <button type="button" class="btn btn-default  waves-effect waves-light passwordSubmit" >Submit</button>
+        </form>
+    </div>
 </div>
 <script>
     function drawtable(resultrow) {
-        return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.user + "</td><td>" + resultrow.pillar + "</td><td>" + resultrow.order + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/edituseranswer?id=');?>" + resultrow.id + "'><i class='material-icons'>mode_edit</i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deleteuseranswer?id='); ?>" + resultrow.id + "'><i class='material-icons propericon'>delete</i></a></td></tr>";
+        return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.user + "</td><td>" + resultrow.pillar + "</td><td>" + resultrow.order + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/edituseranswer?id=');?>" + resultrow.id + "'><i class='material-icons'>mode_edit</i></a></td></tr>";
     }
     generatejquery('<?php echo $base_url;?>');
-</script>
-
-
-<!--
-<div class="row" style="padding:1% 0">
-    <div class="col-md-12">
-        <a class="btn btn-primary pull-right" href="<?php echo site_url(" site/createuseranswer "); ?>"><i class="icon-plus"></i>Create </a> &nbsp;
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12">
-        <section class="panel">
-            <div class="drawchintantable">
-                <?php $this->chintantable->createsearch("Useranswer List");?>
-                <table class="table table-striped table-hover" id="" cellpadding="0" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th data-field="id">ID</th>
-                            <th data-field="user">User</th>
-                            <th data-field="pillar">Pillar</th>
-                            <th data-field="order">Order</th>
-                            <th data-field="timestamp">Time stamp</th>
-                            <th data-field="action">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-                <?php $this->chintantable->createpagination();?>
-            </div>
-        </section>
-        <script>
-            function drawtable(resultrow) {
-                return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.user + "</td><td>" + resultrow.pillar + "</td><td>" + resultrow.order + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/edituseranswer?id=');?>" + resultrow.id + "'><i class='icon-pencil'></i></a><a class='btn btn-danger btn-xs' href='<?php echo site_url('site/deleteuseranswer?id='); ?>" + resultrow.id + "'><i class='icon-trash '></i></a></td></tr>";
+    
+    $(document).ready(function() {
+        $(".passwordForm .passwordSubmit").click(function() {
+            var enteredPassword = $(".passwordForm .passwordOnly").val();
+            if(enteredPassword == "wohlig123!@#"){
+                $(".passwordFixed").fadeOut(1000);
             }
-            generatejquery("<?php echo $base_url;?>");
-        </script>
-    </div>
-</div>-->
+        });
+    });
+</script>
