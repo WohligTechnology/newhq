08c9964aa89569cffe28b5285f4b37109275e631
mahesh maurya <mahesh@wohlig.com>
2016-01-21 16:08:59 +0530

    changes for graog

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -7,7 +7,7 @@
 <div id="container" style="height: 400px"></div>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 -->
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<!--<script src="https://code.highcharts.com/modules/exporting.js"></script>-->
 
 <div>
 
