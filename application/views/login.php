31bae161f416f86fa0aa70487be7b4adcda17c1c
TusharSachde <tushar@wohlig.com>
2016-02-04 19:31:13 +0530

    Login Page Matched

--- application/views/login.php
+++ application/views/login.php
@@ -8,39 +8,88 @@
   <meta name="author" content="Wohlig">
   <title>HQ</title>
   <link rel="shortcut icon" href="<?php echo base_url('assets').'/';?>img/favicon.png" type="image/png" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css"> -->
 
   <style>
+    @font-face {
+        font-family: "Walkway Rounded";
+        src: url('<?php echo base_url('assets').'/';?>fonts/Walkwayrounded.ttf');
+        font-style: normal;
+    }
     body {
-      background-image: url('<?php echo base_url('assets/img/background.jpg'); ?>');
-      background-position: top center;
-      background-size: cover;
-      background-repeat: no-repeat;
+      font-family: 'Walkway Rounded';
+      background-color: #F55069;
+      -webkit-font-smoothing: antialiased;
     }
-    /* label focus color */
-
-    a,
-    .input-field input[type=email]:focus:not([readonly]) + label,
-    .input-field input[type=password]:focus:not([readonly]) + label {
-      color: #546e7a;
-    }
-    /* label underline focus color */
-
-    .input-field input[type=email]:focus:not([readonly]),
-    .input-field input[type=password]:focus:not([readonly]) {
-      border-bottom: 1px solid #e53935;
-      box-shadow: 0 1px 0 0 #e53935;
-    }
-
     .padding-top {
       padding-top: 10%;
     }
-
-    @media only screen and (max-width: 1050px) {
-      body {
-        background-position: top center;
-        background-size: auto;
-      }
+    .text-center {
+      text-align: center;
+    }
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+    }
+    .row {
+      display: flex;
+    }
+    .col {
+      flex: 1;
+    }
+    .border-box {
+      border: 1px solid #EEEC3B;
+      border-radius: 5px;
+      max-width: 300px;
+      margin: 0 auto;
+      padding: 30px 20px;
+    }
+    .input-box+.input-box {
+      margin-top: 20px;
+    }
+    .input-box input {
+      background-color: rgba(0, 0, 0, 0.35);
+      border: none;
+      width: 100%;
+      box-sizing: border-box;
+      padding: 6px 10px;
+      outline: none;
+      color: #EEEC3B;
+      font-size: 14px;
+    }
+    .input-box label {
+      color: #EEEC3B;
+      letter-spacing: 2px;
+      display: block;
+      margin-bottom: 2px;
+    }
+    .custom-btn {
+      border: 1px solid #EEEC3B;
+      background-color: #F55069;
+      color: #EEEC3B;
+      border-radius: 2px;
+      padding: 6px 30px;
+      font-size: 14px;
+      margin-top: 20px;
+      cursor: pointer;
+      outline: none;
+      font-family: inherit;
+      letter-spacing: 2px;
+      box-shadow: -1px 1px 3px rgba(0, 0, 0, 0.35)
+    }
+    .custom-btn:hover {
+      background-color: #DE3D56;
+    }
+    .logo {
+      margin-bottom: 50px;
+    }
+    .logo img {
+      width: 14%;
+    }
+    .guys {
+      width: 75%;
+      margin-left: 30px;
+      max-width: 100%;
     }
   </style>
 
@@ -52,71 +101,39 @@
 
 <body>
   <div class="padding-top">
-    <!--
-        <div style="text-align:center; color: #fff; font-size: 38px">
-            <span>Business</span><span style="color:#f13232">App</span>
-        </div>
--->
+      <form method="post" action="<?php echo site_url('login/validate') ;?>">
 
-    <div class="row">
-      <form class="col s12 offset-s0 m6 offset-m3 l4 offset-l4" method="post" action="<?php echo site_url('login/validate') ;?>">
-        <div class="card">
+        <div class="container">
+          <div class="text-center logo">
+            <img src="<?php echo base_url('assets').'/';?>img/logo-hq.png" alt="" />
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="border-box">
 
-          <!--
+                <div class="input-box">
+                  <label for="email">Email</label>
+                  <input id="email" type="email" name="username">
+                </div>
 
-                        <div class="form-group">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input type="text" class="form-control" name="username" placeholder="Username">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                                <input type="password" class="form-control" name="password" placeholder="Password">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8 text-left checkbox">
-                            </div>
-                            <div class="col-xs-4">
-                                <div class="form-group text-right">
-                                <button class="btn btn-success text-uppercase" type="submit">Sign In</button>
-                                </div>
-                            </div>
-                        </div>
+                <div class="input-box">
+                  <label for="email">Password</label>
+                  <input id="password" type="password" name="password">
+                </div>
+              </div>
 
--->
-          <div class="blue-grey darken-3">
-            <div class="card-content white-text">
-              <span class="card-title">Sign In</span>
+              <div class="text-center">
+                <button class="custom-btn">Login</button>
+              </div>
+            </div>
+            <div class="col">
+              <img src="<?php echo base_url('assets').'/';?>img/login-guys.png" alt="" class="guys"/>
             </div>
           </div>
-
-          <div class="card-content">
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="email" type="email" name="username" class="validate">
-                <label for="email">Email</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="password" type="password" name="password" class="validate">
-                <label for="password">Password</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12">
-                <button class="btn waves-effect waves-light red darken-1 right" type="submit">Login</button>
-                <!--                                <a href="">Forgot password?</a>-->
-              </div>
-            </div>
-          </div>
-
         </div>
+
       </form>
-    </div>
+
   </div>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
